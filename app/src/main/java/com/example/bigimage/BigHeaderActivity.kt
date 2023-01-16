47457268f01c430478012b67d5e6d3d095d247cb
MasoudBB <masoudbahramibidkalme@gmail.com>
2023-01-16 16:52:01 +0330

    Initial commit

---
+++ app/src/main/java/com/example/bigimage/BigHeaderActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bigimage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BigHeaderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bigheader)
+    }
+}
